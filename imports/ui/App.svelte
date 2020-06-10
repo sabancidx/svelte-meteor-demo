1a00631841160cdda09dda9610b9fcf2e5029d2d
Oğuzhan Yıldız <oguzhan.yildiz@sabancidx.com>
2020-06-10 07:01:26 +0300

    Initial

---
+++ imports/ui/App.svelte
@@ -0,0 +1,13 @@
+<script>
+    import { useTracker } from "meteor/rdb:svelte-meteor-data";
+    import { Records } from "../api/records";
+
+    $: records = useTracker(() => Records.find({}).fetch());
+</script>
+
+<header>
+    <h1>Hello New Normal!</h1>
+    {#each $records as record}
+        <h2>{record.title}</h2>
+    {/each}
+</header>
